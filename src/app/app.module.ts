b79e4d27cccf263427211b19272768075123cbc2
Gregory Brinkman <GregoryLucasBrinkman@gmail.com>
2018-03-12 22:28:11 -0500

    Add structural components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RoutingModule } from './routing.module';
 
 
 import { AppComponent } from './app.component';
 import { PiecesComponent } from './pieces/pieces.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SplashComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PiecesComponent
+    PiecesComponent,
+    HeaderComponent,
+    FooterComponent,
+    SplashComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
