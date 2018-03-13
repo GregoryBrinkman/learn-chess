73d5a1f7b79a0e8ff1c9118693e8930a1457f10a
Gregory Brinkman <GregoryLucasBrinkman@gmail.com>
2018-03-13 15:02:48 -0500

    Programmatically generating squares now

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -9,7 +9,23 @@ export class BoardComponent implements OnInit {
 
   constructor() { }
 
+  squares = [];
+
   ngOnInit() {
+    this.makeSquares();
   }
+
+
+  makeSquares() {
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+
+    for (let i = 0; i < letters.length; i++) {
+      for (let j = 0; j < numbers.length; j++) {
+        this.squares.push(letters[i] + '_' + numbers[j]);
+      }
+    }
+  }
+
 
 }
